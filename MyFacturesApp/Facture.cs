09641fe20928919f7cdeb3384d533a0b88f0b3d6
Julien <julien.boisvert2@uqtr.ca>
2024-01-25 13:38:12 -0500

    First commit

--- MyFacturesApp/Facture.cs
+++ MyFacturesApp/Facture.cs
@@ -2,5 +2,15 @@
 
 public class Facture
 {
+    private string _description { get; set; }
+    private float _prixFacture { get; set; }
+    private DateTime _dateFacture { get; set; }
+    
+    public Facture(string description, float prixFacture, DateTime dateFacture)
+    {
+        _description = description;
+        _prixFacture = prixFacture;
+        _dateFacture = dateFacture;
+    }
     
 }
